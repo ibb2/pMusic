35a4722b7670f9121ab1c5087da7aa25a7649980
ibb2 <70052141+ibb2@users.noreply.github.com>
2025-03-18 23:04:36 +1100

    Frame navigation implemented.

--- pMusic/Presentation/MainPage.xaml.cs
+++ pMusic/Presentation/MainPage.xaml.cs
@@ -6,4 +6,26 @@ public sealed partial class MainPage : Page
     {
         this.InitializeComponent();
     }
+
+    private void NavigationView_OnSelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+    {
+
+        if (args.SelectedItemContainer is NavigationViewItem selectedItem)
+        {
+            var selectedTag = selectedItem.Tag.ToString();
+            var pageType = selectedTag switch
+            {
+                "main" => typeof(MainPage),
+                "second" => typeof(SecondPage),
+                // Map other tags to their respective pages
+                _ => null
+            };
+
+            if (pageType != null && ContentFrame.CurrentSourcePageType != pageType)
+            {
+                ContentFrame.Navigate(pageType);
+            }
+        }
+        
+    }
 }
