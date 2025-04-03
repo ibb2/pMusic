de7500028b8b5f86a437380d84079855d266a456
ibb2 <70052141+ibb2@users.noreply.github.com>
2025-04-03 19:24:47 +1100

    Authentication works. Ported from existing pMusic project.

---
+++ pMusic/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml.Linq;
+
+namespace pMusic.ViewModels;
+
+public class LoginViewModel: ViewModelBase
+{
+
+}
--- pMusic/ViewModels/MainViewModel.cs
+++ pMusic/ViewModels/MainViewModel.cs
@@ -1,8 +1,45 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml.Linq;
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using KeySharp;
 
 namespace pMusic.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
+    [ObservableProperty] private bool _isLoggedIn = !string.IsNullOrEmpty(Keyring.GetPassword("com.ib.pmusic-avalonia", "pMusic-Avalonia", "authToken"));
+
+    public void CheckLoginStatus()
+    {
+        string? authToken = null;
+
+        try
+        {
+            authToken = Keyring.GetPassword("com.ib.pmusic-avalonia", "pMusic-Avalonia", "authToken");
+        }
+        catch (Exception ex)
+        {
+            authToken = null;
+        }
+
+        // var auth = services.GetRequiredService<IAuthenticationService>();
+        // var authenticated = await auth.RefreshAsync();
+        if (!string.IsNullOrEmpty(authToken))
+        {
+            IsLoggedIn = true;
+        }
+        else
+        {
+            IsLoggedIn = false;
+        }
+    }
+        
+
 }
