f90acb87cc6eed0a71cf166c5e3c3eb3d41822f7
ibb2 <70052141+ibb2@users.noreply.github.com>
2025-04-05 21:55:53 +1100

    Album page view templating added.

--- pMusic/ViewModels/AlbumViewModel.cs
+++ pMusic/ViewModels/AlbumViewModel.cs
@@ -2,13 +2,16 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace pMusic.ViewModels;
 
-public partial class AlbumViewModel: ViewModelBase
+public partial class AlbumViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    public string _title = "Album";
+    [ObservableProperty] public string _title = "Album";
+    [ObservableProperty] public string _albumTitle = "MUSIC";
+    [ObservableProperty] public string _albumArtist = "Playboi Carti";
+    [ObservableProperty] public string _albumReleaseDate = "2025";
+    [ObservableProperty] public string _albumTrackLength = "30";
+    [ObservableProperty] public string _albumDuration = "1h 16m";
 
     public AlbumViewModel()
     {
-        
     }
 }
