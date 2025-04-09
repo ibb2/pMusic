96dcf6ef8f5c9e8aacdb30d5dcd59a156416e668
ibb2 <70052141+ibb2@users.noreply.github.com>
2025-04-09 15:58:56 +1000

    Play pause button updating on state change.
    Had to make a new converter returning true for paused or stopped state.

--- pMusic/Converter/PlayingConverter.cs
+++ pMusic/Converter/PlayingConverter.cs
@@ -31,3 +31,27 @@ public class PlayingConverter : IValueConverter
         throw new NotImplementedException();
     }
 }
+
+public class PausedConverter : IValueConverter
+{
+    public static readonly PausedConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType,
+        object? parameter, CultureInfo culture)
+    {
+        if (value is PlaybackState state)
+        {
+            Console.WriteLine($"Playback State: {state == PlaybackState.Playing}");
+            return !(state == PlaybackState.Playing);
+
+            // see https://docs.avaloniaui.net/docs/guides/data-binding/how-to-create-a-custom-data-binding-converter
+        }
+
+        return false;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
