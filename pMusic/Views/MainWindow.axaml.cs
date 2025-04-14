ac9e5af98bab20f6eadafa1a2bc135b96fe06afe
ibb2 <70052141+ibb2@users.noreply.github.com>
2025-04-14 13:10:57 +1000

    Installed SukUI and added changed to using its window.

--- pMusic/Views/MainWindow.axaml.cs
+++ pMusic/Views/MainWindow.axaml.cs
@@ -3,12 +3,12 @@ using System.Threading.Tasks;
 using Avalonia.Controls;
 using CommunityToolkit.Mvvm.ComponentModel;
 using KeySharp;
+using SukiUI.Controls;
 
 namespace pMusic.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : SukiWindow
 {
-
     public MainWindow()
     {
         InitializeComponent();
