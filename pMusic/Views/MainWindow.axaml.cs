ff8df0c3ce9c9566c783883274bdfd2d62df5a34
ibb2 <70052141+ibb2@users.noreply.github.com>
2025-04-17 05:01:32 +1000

    Updated avalonia. Suki sidemenu back, customised package.

--- pMusic/Views/MainWindow.axaml.cs
+++ pMusic/Views/MainWindow.axaml.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using KeySharp;
+using pMusic.ViewModels;
 using SukiUI.Controls;
 
 namespace pMusic.Views;
@@ -12,5 +14,22 @@ public partial class MainWindow : SukiWindow
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void GoToAlbum(object? sender, PointerPressedEventArgs e)
+    {
+        if (sender is not SukiSideMenuItem sukiSideMenuItem)
+            return;
+
+        if (sukiSideMenuItem.DataContext is not DisplayAlbumViewModel displayAlbumViewModel)
+            return;
+
+        if (displayAlbumViewModel.Album is null)
+            return;
+
+        if (DataContext is not MainViewModel viewModel)
+            return;
+
+        viewModel.GoToAlbumDetialsPage(displayAlbumViewModel.Album);
     }
 }
