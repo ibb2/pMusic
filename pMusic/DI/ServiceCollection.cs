aeeee114aa6ef5c3df66fc61122e343036b8fd49
ibb2 <70052141+ibb2@users.noreply.github.com>
2025-05-29 15:45:16 +1000

    Improved constant refreshing of homepage after initial navigation.

--- pMusic/DI/ServiceCollection.cs
+++ pMusic/DI/ServiceCollection.cs
@@ -22,7 +22,7 @@ public static class ServiceCollectionExtensions
         collection.AddTransient<IMusic, Music>();
         collection.AddTransient<LoginViewModel>();
         collection.AddTransient<MainViewModel>();
-        collection.AddTransient<HomeViewModel>();
+        collection.AddSingleton<HomeViewModel>();
         collection.AddTransient<AlbumViewModel>();
         collection.AddTransient<ArtistViewModel>();
         collection.AddTransient<SidecarViewModel>();
