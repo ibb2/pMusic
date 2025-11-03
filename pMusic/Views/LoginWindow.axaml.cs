ef6be3d800a9a0109379f1a20194eb39e7e0cc34
ibb2 <70052141+ibb2@users.noreply.github.com>
2025-11-03 21:40:30 +1100

    Sidebar looks more presentable. Non functional however.

--- pMusic/Views/LoginWindow.axaml.cs
+++ pMusic/Views/LoginWindow.axaml.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Threading.Tasks;
 using Avalonia.Controls;
-using Avalonia.Input;
-using CommunityToolkit.Mvvm.ComponentModel;
-using KeySharp;
-using pMusic.ViewModels;
-using SukiUI.Controls;
 
 namespace pMusic.Views;
 
-public partial class LoginWindow : SukiWindow
+public partial class LoginWindow : Window
+
 {
     public LoginWindow()
     {
