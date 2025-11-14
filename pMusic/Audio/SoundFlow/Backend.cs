670979ce0edc0d3e7cccd0b9214469435e867295
ibb2 <70052141+ibb2@users.noreply.github.com>
2025-11-14 22:33:22 +1100

    Added all interface methods for player and backend classes.

--- pMusic/Audio/SoundFlow/Backend.cs
+++ pMusic/Audio/SoundFlow/Backend.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ManagedBass;
+using pMusic.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,45 @@ using System.Threading.Tasks;
 
 namespace pMusic.Audio.SoundFlow;
 
-public class Backend
+public class Backend : IAudioBackend
 {
+    bool IAudioBackend.AdjustVolume(float volume)
+    {
+        throw new NotImplementedException();
+    }
 
+    decimal IAudioBackend.GetLength()
+    {
+        throw new NotImplementedException();
+    }
 
+    decimal IAudioBackend.GetPosition()
+    {
+        throw new NotImplementedException();
+    }
+
+    long IAudioBackend.GetRawLength()
+    {
+        throw new NotImplementedException();
+    }
+
+    double IAudioBackend.GetRawPosition()
+    {
+        throw new NotImplementedException();
+    }
+
+    PlaybackState IAudioBackend.GetState()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IAudioBackend.Init(int stream)
+    {
+        throw new NotImplementedException();
+    }
+
+    void IAudioBackend.Seek(double pos)
+    {
+        throw new NotImplementedException();
+    }
 }
--- pMusic/Audio/SoundFlow/Player.cs
+++ pMusic/Audio/SoundFlow/Player.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using pMusic.Interface;
+using pMusic.Models;
+using pMusic.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace pMusic.Audio.SoundFlow
+namespace pMusic.Audio.SoundFlow;
+
+public class Player : IAudioPlayer
 {
-    internal class Player
+    void IAudioPlayer.Dispose()
     {
+        throw new NotImplementedException();
+    }
+
+    bool IAudioPlayer.Initialize(Plex plex, MusicPlayer musicPlayer, IAudioBackend audioBackend)
+    {
+        throw new NotImplementedException();
+    }
+
+    ValueTask<bool> IAudioPlayer.Pause()
+    {
+        throw new NotImplementedException();
+    }
+
+    bool IAudioPlayer.Play(Track track, string url, string serverUrl)
+    {
+        throw new NotImplementedException();
+    }
+
+    ValueTask<bool> IAudioPlayer.Resume()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IAudioPlayer.Stop()
+    {
+        throw new NotImplementedException();
     }
 }
+
