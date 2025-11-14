cf9d6d85e8d4222c0032f5ac3edb89695c743c03
ibb2 <70052141+ibb2@users.noreply.github.com>
2025-11-14 23:11:10 +1100

    Implemented the init method for soundflow backend

--- pMusic/Audio/SoundFlow/Backend.cs
+++ pMusic/Audio/SoundFlow/Backend.cs
@@ -1,15 +1,36 @@
 ﻿using ManagedBass;
 using pMusic.Interface;
+using SoundFlow.Abstracts.Devices;
+using SoundFlow.Backends.MiniAudio;
+using SoundFlow.Backends.MiniAudio.Devices;
+using SoundFlow.Enums;
+using SoundFlow.Structs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DeviceInfo = SoundFlow.Structs.DeviceInfo;
+using PlaybackState = ManagedBass.PlaybackState;
 
 namespace pMusic.Audio.SoundFlow;
 
 public class Backend : IAudioBackend
 {
+
+    private MiniAudioEngine? _audioEngine;
+    private DeviceInfo _defaultPlaybackDevice;
+    private AudioPlaybackDevice _device;
+
+    void IAudioBackend.Init(int stream)
+    {
+        _audioEngine = new MiniAudioEngine();
+        _defaultPlaybackDevice = _audioEngine.PlaybackDevices.FirstOrDefault(d => d.IsDefault);
+        var audioFormat = AudioFormat.StudioHq;
+
+        _device = _audioEngine.InitializePlaybackDevice(_defaultPlaybackDevice, audioFormat);
+    }
+
     bool IAudioBackend.AdjustVolume(float volume)
     {
         throw new NotImplementedException();
@@ -40,13 +61,9 @@ public class Backend : IAudioBackend
         throw new NotImplementedException();
     }
 
-    void IAudioBackend.Init(int stream)
-    {
-        throw new NotImplementedException();
-    }
-
     void IAudioBackend.Seek(double pos)
     {
         throw new NotImplementedException();
     }
+
 }
